9b9d6a65d9795f77f27fc2850eaf41937297a7be
nasre674 <tekomeg374@gmail.com>
2026-04-19 02:37:56 -0400

    Ajout de la methode get_connection dans DataSalle

---
+++ Data/dao_salle.py
@@ -0,0 +1,13 @@
+import json
+import mysql.connector
+class DataSalle:
+    def get_connection(self):
+        with open("Data/config.json", "r", encoding="utf-8") as fichier:
+            config = json.load(fichier)
+        connexion = mysql.connector.connect(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+        return connexion
