a54db88f2574ccf1884facedd91cd5bcdb8d0157
nasre674 <tekomeg374@gmail.com>
2026-04-19 04:00:17 -0400

    Ajout du test ajouter une salle dans main

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ connexion.close()
 salle1 = Salle("A102", "Salle Info", "Laboratoire", 30)
 dao.insert_salle(salle1)
 print("Salle ajoutée")
+
+from Data.dao_salle import DataSalle
+from models.salle import Salle
+
+dao = DataSalle()
+
+# Tester la connexion à la base de données
+connexion = dao.get_connection()
+if connexion.is_connected():
+    print("Connexion réussie")
+connexion.close()
+
+# Ajouter une salle
+salle1 = Salle("A102", "Salle Info", "Laboratoire", 30)
+dao.insert_salle(salle1)
+print("Salle ajoutée")
