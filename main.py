63045abd1cf5a9869ca27b5590a9cc9b18b75978
nasre674 <tekomeg374@gmail.com>
2026-04-19 04:15:18 -0400

    Test de connexion de la couche DAO dans main

--- main.py
+++ main.py
@@ -1,83 +1,8 @@
 from Data.dao_salle import DataSalle
-from models.salle import Salle
 
 dao = DataSalle()
 
-# Tester la connexion à la base de données
 connexion = dao.get_connection()
 if connexion.is_connected():
     print("Connexion réussie")
 connexion.close()
-
-# Ajouter une salle
-salle1 = Salle("A102", "Salle Info", "Laboratoire", 30)
-dao.insert_salle(salle1)
-print("Salle ajoutée")
-
-from Data.dao_salle import DataSalle
-from models.salle import Salle
-
-dao = DataSalle()
-
-# Tester la connexion à la base de données
-connexion = dao.get_connection()
-if connexion.is_connected():
-    print("Connexion réussie")
-connexion.close()
-
-# Ajouter une salle
-salle1 = Salle("A102", "Salle Info", "Laboratoire", 30)
-dao.insert_salle(salle1)
-print("Salle ajoutée")
-
-from Data.dao_salle import DataSalle
-from models.salle import Salle
-
-dao = DataSalle()
-
-# Tester la connexion
-connexion = dao.get_connection()
-if connexion.is_connected():
-    print("Connexion réussie")
-connexion.close()
-
-# Ajouter une salle
-salle1 = Salle("A103", "Salle Info", "Laboratoire", 30)
-dao.insert_salle(salle1)
-print("Salle ajoutée")
-
-# Modifier une salle
-salle1.libelle = "Salle Informatique"
-salle1.type = "Laboratoire"
-salle1.capacite = 35
-dao.update_salle(salle1)
-print("Salle modifiée")
-
-from Data.dao_salle import DataSalle
-from models.salle import Salle
-
-dao = DataSalle()
-
-# Tester la connexion
-connexion = dao.get_connection()
-if connexion.is_connected():
-    print("Connexion réussie")
-connexion.close()
-
-# Ajouter une salle
-salle1 = Salle("A103", "Salle Info", "Laboratoire", 30)
-dao.insert_salle(salle1)
-print("Salle ajoutée")
-
-# Modifier une salle
-salle1.libelle = "Salle Informatique"
-salle1.type = "Laboratoire"
-salle1.capacite = 35
-dao.update_salle(salle1)
-print("Salle modifiée")
-
-# Rechercher une salle par son code
-salle_trouvee = dao.get_salle("A103")
-if salle_trouvee:
-    print("Salle trouvée :")
-    salle_trouvee.afficher_infos()
