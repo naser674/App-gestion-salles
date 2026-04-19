8b27533a62c9b6612abb9f87118de7a8b58717af
nasre674 <tekomeg374@gmail.com>
2026-04-19 04:33:41 -0400

    Creation de la classe ServiceSalle avec l attribut dao_salle

--- models/salle.py
+++ models/salle.py
@@ -1,11 +1,12 @@
 class Salle:
-    def _init_(self, code, libelle, type, capacite):
+    def __init__(self, code="", libelle="", type="", capacite=0):
         self.code = code
         self.libelle = libelle
         self.type = type
         self.capacite = capacite
+
     def afficher_infos(self):
-        print("code:", self.code)
-        print("libelle:", self.libelle)
-        print("type:", self.type)
-        print("capacite:", self.capacite)
+        print(f"Code : {self.code}")
+        print(f"Libellé : {self.libelle}")
+        print(f"Type : {self.type}")
+        print(f"Capacité : {self.capacite}")
