48ca7cf2526c05e5ad51141fdacea83f7282c27b
naser674 <tekomeg374@gmail.com>
2026-04-19 21:13:13 -0400

    Update salle.py

--- models/salle.py
+++ models/salle.py
@@ -1,12 +1,9 @@
 class Salle:
-    def __init__(self, code="", libelle="", type="", capacite=0):
+    def __init__(self, code, libelle, type, capacite):
         self.code = code
         self.libelle = libelle
         self.type = type
         self.capacite = capacite
 
     def afficher_infos(self):
-        print(f"Code : {self.code}")
-        print(f"Libellé : {self.libelle}")
-        print(f"Type : {self.type}")
-        print(f"Capacité : {self.capacite}")
+        return f"Code: {self.code} | Libellé: {self.libelle} | Type: {self.type} | Capacité: {self.capacite}"
