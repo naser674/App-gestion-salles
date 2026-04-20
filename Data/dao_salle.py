43865d28fdbefc04d6b3923ead55cbd3d52096c2
naser674 <tekomeg374@gmail.com>
2026-04-19 21:12:35 -0400

    Update dao_salle.py

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -1,63 +1,76 @@
 import json
 import mysql.connector
-
 from models.salle import Salle
 
 
 class DataSalle:
+    def __init__(self):
+        self.config_path = "data/config.json"
+
     def get_connection(self):
-        with open("Data/config.json", "r", encoding="utf-8") as fichier:
-            config = json.load(fichier)
-        connexion = mysql.connector.connect(
+        with open(self.config_path, "r", encoding="utf-8") as f:
+            config = json.load(f)
+
+        conn = mysql.connector.connect(
             host=config["host"],
             user=config["user"],
             password=config["password"],
             database=config["database"]
         )
-        return connexion
-    def insert_salle(self,salle):
-        connexion = self.get_connection()
-        cursor = connexion.cursor()
-        requete ="""
-        INSERT INTO salle (code, libelle, type, capacite)
-        VALUES (%s, %s, %s, %s)
-        """
-        valeurs = (salle.code, salle.libelle, salle.type, salle.capacite)
-        cursor.execute(requete, valeurs)
-        connexion.commit()
+        return conn
+
+    def insert_salle(self, salle):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        sql = "INSERT INTO Salle (code, libelle, type, capacite) VALUES (%s, %s, %s, %s)"
+        values = (salle.code, salle.libelle, salle.type, salle.capacite)
+        cursor.execute(sql, values)
+        conn.commit()
         cursor.close()
-        connexion.close()
+        conn.close()
+
+    def update_salle(self, salle):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        sql = "UPDATE Salle SET libelle=%s, type=%s, capacite=%s WHERE code=%s"
+        values = (salle.libelle, salle.type, salle.capacite, salle.code)
+        cursor.execute(sql, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
     def delete_salle(self, code):
-        connexion = self.get_connection()
-        cursor = connexion.cursor()
-        requete = "DELETE FROM salle WHERE code = %s"
-        cursor.execute(requete, (code,))
-        connexion.commit()
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        sql = "DELETE FROM Salle WHERE code=%s"
+        cursor.execute(sql, (code,))
+        conn.commit()
         cursor.close()
-        connexion.close()
+        conn.close()
 
     def get_salle(self, code):
-        connexion = self.get_connection()
-        cursor = connexion.cursor()
-
-        requete = "SELECT code, libelle, type, capacite FROM salle WHERE code = %s"
-        cursor.execute(requete, (code,))
-        resultat = cursor.fetchone()
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        sql = "SELECT code, libelle, type, capacite FROM Salle WHERE code=%s"
+        cursor.execute(sql, (code,))
+        row = cursor.fetchone()
         cursor.close()
-        connexion.close()
-        if resultat:
-            return Salle(resultat[0], resultat[1], resultat[2], resultat[3])
+        conn.close()
+
+        if row:
+            return Salle(row[0], row[1], row[2], row[3])
         return None
-    def get_salles(self):
-        connexion = self.get_connection()
-        cursor = connexion.cursor()
-        requete = "SELECT code, libelle, type, capacite FROM salle"
-        cursor.execute(requete)
-        resultat = cursor.fetchall()
-        cursor.close()
-        connexion.close()
-        liste_salles = []
-        for row in resultats:
-            liste_salles.append(Salle(row[0], row[1], row[2], row[3]))
 
-        return liste_salles
+    def get_salles(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        sql = "SELECT code, libelle, type, capacite FROM Salle"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        salles = []
+        for row in rows:
+            salles.append(Salle(row[0], row[1], row[2], row[3]))
+        return salles
